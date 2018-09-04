db4e541d7d9b532f791ddecc1e50cf5997c841f3
Nikita Rotsky <kurt.rotzke@gmail.com>
2018-09-04 21:09:51 +0300

    Fixed regexes

--- datahunter.py
+++ datahunter.py
@@ -12,18 +12,29 @@ logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s',
 
 FOLDER = "Identity_Finder_Test_Data"
 
-CC_PATTERN = '''(?:
-(?P<visa>4[0-9]{12}(?:[0-9]{3})?) |
-(?P<mastercard>5[1-5][0-9]{14}) |
-(?P<discover>6(?:011|5[0-9]{2})[0-9]{12}) |
-(?P<amex>3[47][0-9]{13}) |
-(?P<diners>3(?:0[0-5]|[68][0-9])[0-9]{11}) |
-(?P<jcb>(?:2131|1800|35[0-9]{3})[0-9]{11})
-)'''
+CC_PATTERN = '''(?:4[0-9]{12}(?:[0-9]{3})?|(?:5[1-5][0-9]{2}
+|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}
+|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011
+|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11})'''
 cc_regex = re.compile(CC_PATTERN)
 
-SSN_PATTERN = '([1-9])(?!\1{2}-\1{2}-\1{4})[1-9]{2}-[1-9]{2}-[1-9]{4}'
+SSN_PATTERN = '''^(?!b(d)1+-(d)1+-(d)1+b)(?!123-45-6789
+|219-09-9999|078-05-1120)(?!666|000|9d{2})d{3}-(?!00)d{2}-(?!0{4})d{4}$'''
 ssn_regex = re.compile(SSN_PATTERN)
+
+
+# def unpack_zip(filename, dir_to, test_id, test_run_id):
+#     def get_root_dir_from_zip(zipIO):
+#         zip_inside = (x for x in zipIO.namelist())
+#         zip_first = next(zip_inside)
+#         first_dir = zip_first[:zip_first.find('/') + 1]
+#         return first_dir
+#
+#     with zipfile.ZipFile(filename, 'r') as z:
+#         dir = get_root_dir_from_zip(z)
+#         dirname = check_dir(dir_to, test_id, test_run_id)
+#         z.extractall(dirname)
+#     return dirname + dir
 
 # TODO Add zip files handler
 files = [os.path.join(dp, f) for dp, dn, fn in
@@ -31,8 +42,9 @@ files = [os.path.join(dp, f) for dp, dn, fn in
 
 # TODO Add .mdb support
 for f in files:
-    try:
-        body = textract.process(f)
-    except textract.exceptions.ExtensionNotSupported:
-        logging.warning('Extension is not supported!')
-    print(re.findall(cc_regex, body))
+    if 'fake_ssn.txt' in f:
+        try:
+            body = textract.process(f)
+        except textract.exceptions.ExtensionNotSupported:
+            logging.warning('Extension is not supported!')
+        print(ssn_regex.findall(body))
